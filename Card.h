97fc65d3af1356211afe0e3750201dc5266a6df7
Mahrokh-M <mahrokhmousavii44@gmail.com>
2023-06-03 19:23:18 +0330

    cards class updated

--- Card.h
+++ Card.h
@@ -1,18 +1,15 @@
 #pragma once
 #include<iostream>
+#include"topCard.h"
 using namespace std;
-class Card
-{private:
-	string type; //Queen King Pirate flag parrot treasure map 
+class Card:public topCard
+{
+private:
 	int number;
-	int value;
-	bool isReserved;
 public:
 	Card();
 	Card(string, int, int, bool);
-	string get_type();
-	int get_number();
-	int get_value();
-	bool & set_get_isReserved();
+	int&get_number();
+
 };
 
