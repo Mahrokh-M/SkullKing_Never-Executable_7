ac8f32fa700533310abdaa895b04ff6b01f716b1
Saba-Ra <133780497+Saba-Ra@users.noreply.github.com>
2023-06-21 12:41:11 +0330

    Update User.h

--- User.h
+++ User.h
@@ -5,27 +5,29 @@
 class User
 {
 private:
-    int avatar;
     QString name;
     QString userName;
     QString password;
     QString phoneNumber;
+    QString email;
     QString money;
     int total_wins;
     int total_lose;
+    int avatar;
     //vector<History> all_games;
     //list<Card> cards;
 public:
     User();
-    User(QString given_name, QString given_userName, QString given_password, QString given_phoneNumber, QString given_money, int given_avatar);
+    User(QString given_name, QString given_userName, QString given_password, QString given_phoneNumber, QString given_money);
     int& set_get_total_win();
     int& set_get_total_lose();
+    int& set_get_avatar();
     QString& set_get_name();
     QString& set_get_userName();
     QString& set_get_password();
     QString& set_get_phoneNumber();
+    QString& set_get_email();
     QString& set_get_money();
-    int& set_get_avatar();
     //vector<History>& set_get_History();
     //list<Card>& set_get_cards();
 };
