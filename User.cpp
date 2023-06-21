b5040525962ad82b18cceb0c4af27c50e8e1c869
Saba-Ra <133780497+Saba-Ra@users.noreply.github.com>
2023-06-21 12:40:11 +0330

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -8,25 +8,25 @@ User::User() {
     money = "";
     total_wins = 0;
     total_lose = 0;
-    avatar=0;
+    avatar= 0;
 }
-User::User(QString given_name, QString given_userName, QString given_password, QString given_phoneNumber, QString given_money, int given_avatar) {
+User::User(QString given_name, QString given_userName, QString given_password, QString given_phoneNumber, QString given_money) {
     name = given_name;
     userName = given_userName;
     password = given_password;
-    phoneNumber = given_money;
+    phoneNumber = given_phoneNumber;
     money = given_money;
     total_wins = 0;
     total_lose = 0;
-    avatar=given_avatar;
 }
 QString& User::set_get_name() { return name; }
 QString& User::set_get_userName() { return userName; }
 QString& User::set_get_password() { return password; }
 QString& User::set_get_phoneNumber() { return phoneNumber; }
+QString& User::set_get_email() { return email; }
 QString& User::set_get_money() { return money; }
 int& User::set_get_total_win() { return total_wins; }
 int& User::set_get_total_lose() { return total_lose; }
-int& User::set_get_avatar(){return avatar;}
+int& User::set_get_avatar() { return avatar; }
 //vector<History>& User::set_get_History(){return all_games;}
 //list<Card>& User::set_get_cards(){return cards;}
